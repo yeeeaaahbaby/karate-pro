474563e1aeaa55e15f557eddaf42b0a344353465
Vorace <mac@MacBookPro.lan>
2026-06-16 13:07:57 +0200

    fix: use new OneSignal API endpoint

--- functions/index.js
+++ functions/index.js
@@ -21,7 +21,7 @@ async function sendOneSignalPush(title, body) {
     return { skipped: true, reason: "no_players" };
   }
 
-  const response = await fetch("https://onesignal.com/api/v1/notifications", {
+  const response = await fetch("https://api.onesignal.com/notifications", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
