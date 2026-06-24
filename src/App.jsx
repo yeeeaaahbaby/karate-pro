8a3ebd63b60f3c1ee7247fcc183bc1ffbb64f2af
Vorace <mac@MacBookPro.lan>
2026-06-24 12:35:38 +0200

    v35b: fix useEffect PPG v5 non fermé

--- src/App.jsx
+++ src/App.jsx
@@ -3566,6 +3566,7 @@ export default function App() {
       } catch(e) { console.error("Seed PPG v5:", e); }
     };
     init();
+  }, [authUser]);
 
   // Plannings hebdos : charger depuis Firestore
   useEffect(() => {
